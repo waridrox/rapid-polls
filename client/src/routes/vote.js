af50ff82f9c89bc0460e402b816f811913b81bc5
waridrox <mohdwarid4@gmail.com>
2022-05-23 20:06:52 +0530

    added vote page route

---
+++ client/src/routes/vote.js
@@ -0,0 +1,11 @@
+import PollVote from '../components/PollVote'
+
+const Vote = () => {
+  return (
+    <div className="container mt-4">
+      <PollVote/>
+    </div>
+  )
+}
+
+export default Vote
