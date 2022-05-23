c4d8e6fe78b1b17da30e5e0e86f80892fe80338e
waridrox <mohdwarid4@gmail.com>
2022-05-23 13:42:03 +0530

    implemented routing using react-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+import App from './App'
+import NotFound from './components/NotFound'
+
+import Create from './routes/create'
+import Manage from './routes/manage'
+
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'react-toastify/dist/ReactToastify.css'
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}>
+        <Route path="/create" element={<Create/>}/>
+        <Route path="/manage/:id" element={<Manage/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
