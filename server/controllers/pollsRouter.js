29f827df7ce611fd39fe0e0603ba8aaa3268da2e
waridrox <mohdwarid4@gmail.com>
2022-05-24 20:31:35 +0530

    added ability to update or toggle poll states

--- server/controllers/pollsRouter.js
+++ server/controllers/pollsRouter.js
@@ -13,13 +13,30 @@ pollsRouter.get('/:id', async (request, response) => {
 
 pollsRouter.put('/:id', async (request, response) => {
   const { id } = request.params
-  const updated = await Poll.findByIdAndUpdate(id, request.body, { new: true, runValidators: true })
+  const { question, options, state, totalVotes } = request.body
+
+  if (!(question && options && state && totalVotes !== null && totalVotes != undefined)) {
+    return response.status(400).json({ error: 'one or more poll properties are missing' })
+  }
+
+  const existing = await Poll.findById(id)
+  if (existing.state === 'ended') {
+    return response.status(400).json({ error: 'poll has already ended' })
+  }
+
+  options.forEach((option) => {
+    option._id = option.id
+    delete option.id
+  })
+
+  const modified = { question, options, state, totalVotes }
+  const updated = await Poll.findByIdAndUpdate(id, modified, { new: true, runValidators: true })
   response.json(updated)
 })
 
 pollsRouter.post('/', async (request, response) => {
-  const { question, options, active } = request.body
-  const poll = new Poll({ question, options, active })
+  const { question, options, state } = request.body
+  const poll = new Poll({ question, options, state })
   const saved = await poll.save()
   response.status(201).json(saved)
 })
