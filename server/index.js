b11f337695858185f1723b09f0aebaa9e085fd17
waridrox <mohdwarid4@gmail.com>
2022-05-25 16:43:17 +0530

    refactored server initialisation code to avoid circular dependency

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
-const server = require('./servers/http')
+const http = require('http')
+const { Server } = require('socket.io')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const app = require('./app')
+
+const server = http.createServer(app)
+const io = new Server(server)
 
 server.listen(config.PORT, () => {
   logger.info('Server listening on port', config.PORT)
 })
+
+io.on('connection', (socket) => {
+  socket.emit('send-poll-id')
+  socket.on('poll-id-sent', (id) => socket.join(id))
+})
+
+global.io = io
