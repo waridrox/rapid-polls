03b6605a87a7b0b5efadb259ead75a3109f2a371
waridrox <mohdwarid4@gmail.com>
2022-05-25 22:06:16 +0530

    migrated to redis labs instance

--- server/databases/redis.js
+++ server/databases/redis.js
@@ -1,5 +1,9 @@
-const client = require('redis').createClient()
+const config = require('../utils/config')
 const logger = require('../utils/logger')
+
+const client = require('redis').createClient({
+  url: `redis://${config.REDIS_USERNAME}:${config.REDIS_PASSWORD}@${config.REDIS_HOST}`
+})
 
 const connectToRedis = () => {
   client.connect()
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 
-const { PORT, MONGO_URI } = process.env
+const {
+  PORT,
+  MONGO_URI,
+  REDIS_USERNAME,
+  REDIS_PASSWORD,
+  REDIS_HOST,
+} = process.env
 
 module.exports = {
   PORT,
-  MONGO_URI
+  MONGO_URI,
+  REDIS_USERNAME,
+  REDIS_PASSWORD,
+  REDIS_HOST,
 }
