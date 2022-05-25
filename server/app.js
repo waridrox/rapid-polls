6237a42a5ef367715919f6e75bc2d8197f3bbff8
waridrox <mohdwarid4@gmail.com>
2022-05-25 19:03:19 +0530

    added rendering of static assets (react build)

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require('express-async-errors')
 const cors = require('cors')
 const express = require('express')
+const path = require('node:path')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const pollsRouter = require('./controllers/pollsRouter')
@@ -18,9 +19,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.use('/api/polls', pollsRouter)
 app.use('/api/votes', votesRouter)
+
+app.get('/*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
