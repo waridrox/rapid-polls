73ef5aee4db15cdfc3e3afe9204df32251b58cb2
waridrox <mohdwarid4@gmail.com>
2022-05-26 06:04:12 +0530

    added missing link to create route

--- client/src/components/home/GetStarted.js
+++ client/src/components/home/GetStarted.js
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const GetStarted = () => {
   return (
     <div className="mt-5 text-center">
       <h2 className="mt-2 text-center">Ready to create a poll?</h2>
-      <button className="btn btn-success btn-lg mt-3">Get started right away!</button>
+      <Link to="/create">
+        <button className="btn btn-success btn-lg mt-3">Get started right away!</button>
+      </Link>
     </div>
   )
 }
