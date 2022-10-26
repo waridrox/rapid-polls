fd293da5f7c5f86b854e9873264334780d04c07e
waridrox <mohdwarid4@gmail.com>
2022-10-26 17:10:06 +0530

    updated footer

--- client/src/components/home/Footer.js
+++ client/src/components/home/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="container col-12 text-center mt-5">
-      Made with <span className="bi bi-heart-fill"></span> by <a target="_blank" rel="noreferrer" className="link-dark" href="https://github.com/dwrik">dwrik</a><br/>
+      Made with <span className="bi bi-heart-fill"></span> by <a target="_blank" rel="noreferrer" className="link-dark" href="https://github.com/dwrik">dwrik</a><br/><br/>
     </footer>
   )
 }
