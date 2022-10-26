3d2b28575aa3807fc1d9c5651353186503debabc
waridrox <mohdwarid4@gmail.com>
2022-10-26 17:04:06 +0530

    updated footer

--- client/src/components/home/Footer.js
+++ client/src/components/home/Footer.js
@@ -2,7 +2,6 @@ const Footer = () => {
   return (
     <footer className="container col-12 text-center mt-5">
       Made with <span className="bi bi-heart-fill"></span> by <a target="_blank" rel="noreferrer" className="link-dark" href="https://github.com/dwrik">dwrik</a><br/>
-      <a target="_blank" rel="noreferrer" className="text-muted small mt-2" href="https://www.flaticon.com/free-icons/graph" title="graph icons">graph icons created by freepik - flaticon</a>
     </footer>
   )
 }
